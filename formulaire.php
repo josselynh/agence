1b5a9bbaaa622a844af1c119b1f128e62be44d49
SimplonTroyes <xdarras@simplon.co>
2016-07-25 11:29:56 +0200

    Update formulaire.php

--- formulaire.php
+++ formulaire.php
@@ -9,8 +9,8 @@ session_start(); // démarrage de la session
       <link rel="stylesheet" href="style.css" type="text/css" media="screen"/>
   </head>
   <body><br><br><br>
-        <h2 align= "center"> Votre agence de voyage à votre disposition <br>
-        <h3 align="center"> Identifiez vous et faites votre choix !<br><br>
+        <h2 align= "center"> Votre agence de voyage à votre disposition </h2>
+        <h3 align="center"> Identifiez vous et faites votre choix !</h3>
         <select name="civilite">
             <option value="value1">Mr</option>
             <option value="value2">Mme</option>
@@ -18,10 +18,10 @@ session_start(); // démarrage de la session
         <form action="agence.php" method="post">
             <p> Nom : <input name="nom" required size="30" type="text"  </p>
             <p> Prenom : <input type="text" size="30" required name="prenom"></p>
-            <p style="width:200px;"><font color="#C83D87"><input type=radio required name="choix" value="monvoyage"> Voyage</p>
-            <p style="width:200px;"><font color="#C83D87"><input type=radio required name="choix" value="excursion"> Excursion</font></p>
+            <p style="width:200px;"><font color="#C83D87"><input type="radio" required name="choix" value="monvoyage"> Voyage</p>
+            <p style="width:200px;"><font color="#C83D87"><input type="radio" required name="choix" value="excursion"> Excursion</font></p>
             <p style="width:30px;"><input type=submit name="suite" value="suite"></p>
-        </form> </h3></h2><br><br><br><br><br><br>
+        </form> <br><br><br><br><br><br>
         "<marquee><h1> BIEVENUE &nbsp;&nbsp DANS &nbsp;&nbsp VOTRE &nbsp;&nbsp AGENCE &nbsp;&nbsp DE  &nbsp VOYAGE</marquee></h1>";
   </body>
 </html>
